104fc7b15f697cccf2f2a5c0c35481c416c90217
syaifuladala <adalasyaiful@gmail.com>
2019-06-12 16:56:22 +0700

    tambah tampilan kebencanaan

---
+++ geobuild.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Bulan Mei 2019 pada 09.38
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `geobuild`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `gambar`
+--
+
+CREATE TABLE `gambar` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `gambar`
+--
+
+INSERT INTO `gambar` (`id`, `nama`) VALUES
+(111, 'proyeksi_geobuild.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `gambar`
+--
+ALTER TABLE `gambar`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
